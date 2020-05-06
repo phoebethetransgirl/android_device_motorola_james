ca287a2e4eb95fe4a7969acf8e3548a05d7fd530
wh0dat <whodat@remainsilent.net>
2020-05-06 12:30:27 -0500

    james: Update check_device.sh for james EGL blobs
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,22 +2,7 @@
 
 SKU=`getprop ro.boot.hardware.sku`
 
-if [ "$SKU" = "XT1924-6" ] || [ "$SKU" = "XT1924-7" ] || [ "$SKU" = "XT1924-8" ]; then
-    rm -rf /system/priv-app/MotoDoze
-    rm /vendor/etc/audio_platform_info_ahannah.xml
-    rm /vendor/etc/mixer_paths_ahannah.xml
-    rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
-else
-    mv /vendor/etc/audio_platform_info_ahannah.xml /vendor/etc/audio_platform_info.xml
-    mv /vendor/etc/mixer_paths_ahannah.xml /vendor/etc/mixer_paths.xml
-    mv /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf /vendor/etc/sensors/sensor_def_qcomdev.conf
-    rm -rf /vendor/app/LineageActions
-    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
-    rm /vendor/etc/permissions/android.hardware.sensor.gyroscope.xml
-fi
-
-    # Replace EGL for rhannah variants
-if [ "$SKU" = "XT1924-1" ] || [ "$SKU" = "XT1924-4" ] || [ "$SKU" = "XT1924-5" ]; then
+if [ "$SKU" = "XT1921-3" ]; then
     mv /vendor/lib/egl/libEGL_adreno_8917.so /vendor/lib/egl/libEGL_adreno.so
     mv /vendor/lib/egl/libGLESv1_CM_adreno_8917.so /vendor/lib/egl/libGLESv1_CM_adreno.so
     mv /vendor/lib/egl/libGLESv2_adreno_8917.so /vendor/lib/egl/libGLESv2_adreno.so
