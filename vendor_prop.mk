9a3298da7e5235ef0ecfb8d7b9f6ab929f211865
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-23 12:55:11 +0300

    james: rootdir: Update props for james_t
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,3 +1,22 @@
-# Camera
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.tof.direct=1
+    persist.vendor.audio.fluence.speaker=false
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=2
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false
+
+# GPU
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196608
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.sensors=james \
+    ro.vendor.sensors.amd=false \
+    ro.vendor.sensors.gravity=false \
+    ro.vendor.sensors.laccel=false
