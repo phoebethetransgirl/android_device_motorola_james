fbfdd57e1c72c4a2a1613e08eda68bc13717e59a
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-23 12:55:11 +0300

    james: rootdir: Update init script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ide343673c74917daef6b05547e02f47a699ea433
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,10 +6,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=2
 
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false
+
 # GPU
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.sensors=james
+    ro.hardware.sensors=james \
+    ro.vendor.sensors.amd=false \
+    ro.vendor.sensors.gravity=false \
+    ro.vendor.sensors.laccel=false
