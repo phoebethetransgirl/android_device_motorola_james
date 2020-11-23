11d064ea4e5b010658c3ab97b6c59fe92619b8cf
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-23 12:48:52 +0300

    james: Set decommonized props
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Idffb90369f100ffba7944a08bd55169f68810067

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,3 +1,15 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.speaker=false
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=2
+
+# GPU
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196608
+
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.sensors=james
