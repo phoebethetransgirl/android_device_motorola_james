b31567505029954aaa8eeab26c4b8b99549fc503
Trulan <trulanm@gmail.com>
2021-05-28 19:55:48 -0400

    james: Enable fingerprint sensor for XT1921-1

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -7,6 +7,10 @@ rm /vendor/etc/permissions/android.hardware.vulkan.compute-0.xml
 rm /vendor/etc/permissions/android.hardware.vulkan.level-0.xml
 rm /vendor/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
+if [ "$SKU" = "XT1921-1" ]; then
+    rm /vendor/etc/permissions/unavail.android.hardware.fingerprint.xml
+fi
+
 if [ "$SKU" = "XT1921-2" ]; then
     rm /vendor/etc/permissions/unavail.android.hardware.fingerprint.xml
     rm /vendor/etc/permissions/unavail.android.hardware.gyroscope.xml
