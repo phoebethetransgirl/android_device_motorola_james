4e9175add200d53200ecafea34732af4a65898a3
phoebethetransgirl <101948429+phoebethetransgirl@users.noreply.github.com>
2022-03-19 12:33:21 -0700

    Update and rename lineage_james.mk to kasumi_james.mk
    Update lineage_james to work for Project Kasumi

--- kasumi_james.mk
+++ kasumi_james.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/kasumi/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_james
+PRODUCT_NAME := kasumi_james
 PRODUCT_DEVICE := james
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e5 play
